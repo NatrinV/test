906f3c91052c2558e451406ba396eeec10f88b0c
Vavan Nat <v.a.natrin@mail.ru>
2022-04-30 13:02:02 +0300

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Vladimir Natrin on 30.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
